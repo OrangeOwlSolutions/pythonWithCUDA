488db8eb309bb591b2347bdabc88955f9f4bb57e
Orange Owl <orangeowlsolutions@gmail.com>
2019-01-29 12:41:26 +0100

    Update vectorSum_v3.py

--- vectorSum_v3.py
+++ vectorSum_v3.py
@@ -5,7 +5,14 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy as np
 
-N = 10000
+########
+# MAIN #
+########
+
+start = cuda.Event()
+end   = cuda.Event()
+
+N = 100000
 
 BLOCKSIZE = 256
 
@@ -20,7 +27,15 @@ h_c = np.empty_like(h_a)
 
 d_a = gpuarray.to_gpu(h_a)
 d_b = gpuarray.to_gpu(h_b)
-h_c = (d_a + d_b).get()
+
+start.record()
+d_c = (d_a + d_b)
+end.record() 
+end.synchronize()
+secs = start.time_till(end) * 1e-3
+print("Processing time = %fs" % (secs))
+
+h_c = d_c.get()
 
 if np.array_equal(h_c, h_a + h_b):
   print("Test passed!")
