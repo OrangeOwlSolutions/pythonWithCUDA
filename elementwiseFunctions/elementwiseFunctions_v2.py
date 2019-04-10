79e67ff558b040fc2e32ee6787636b8822518124
Orange Owl <orangeowlsolutions@gmail.com>
2019-04-10 12:00:58 +0200

    Add files via upload

---
+++ elementwiseFunctions/elementwiseFunctions_v2.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+# --- PyCUDA initialization
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+###################
+# iDivUp FUNCTION #
+###################
+def iDivUp(a, b):
+    return a // b + 1
+
+########
+# MAIN #
+########
+
+start = cuda.Event()
+end   = cuda.Event()
+
+N = 100000
+
+BLOCKSIZE = 256
+
+# --- Create random vectorson the CPU
+h_a = np.random.randn(1, N)
+h_b = np.random.randn(1, N)
+
+# --- Set CPU arrays as single precision
+h_a = h_a.astype(np.float32)
+h_b = h_b.astype(np.float32)
+h_c = np.empty_like(h_a)
+
+mod = SourceModule("""
+__global__ void deviceElementwise(float * __restrict__ d_c, const float * __restrict__ d_a, 
+                                                    const float * __restrict__ d_b,
+                                                    const int N)
+{
+  const int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  if (tid >= N) return;
+  d_c[tid] = sqrt(fabs(d_a[tid])) + exp(d_b[tid]);
+}
+""")
+
+d_a = gpuarray.to_gpu(h_a)
+d_b = gpuarray.to_gpu(h_b)
+d_c = gpuarray.zeros_like(d_a)
+
+# --- Define a reference to the __global__ function and call it
+deviceElementwise = mod.get_function("deviceElementwise")
+blockDim          = (BLOCKSIZE, 1, 1)
+gridDim           = (iDivUp(N, BLOCKSIZE), 1, 1)
+start.record()
+deviceElementwise(d_c, d_a, d_b, np.int32(N), block = blockDim, grid = gridDim)
+end.record() 
+end.synchronize()
+secs = start.time_till(end) * 1e-3
+print("Processing time = %fs" % (secs))
+
+h_c = d_c.get()
+
+if np.all(abs(h_c - (np.sqrt(np.abs(h_a)) + np.exp(h_b))) < 1e-5):
+  print("Test passed!")
+else :
+  print("Error!")
+
+# --- Flush context printf buffer
+cuda.Context.synchronize()
